e449e663bb08b466dead1237354161c3f27b9661
LucasR29 <lrigon2904@gmail.com>
2025-01-13 12:31:56 -0300

    add owner

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,19 +1,19 @@
 import hre from "hardhat";
 
 export async function main() {
-    console.log('Deploying AnyFlowERC721...')
+    console.log('Deploying AnyFlowERC20...')
 
     const args = [
         '0xA71a71A71a71a71A71a71a71a71A71a71A71A71a',   // _initialOwner
-        'AnyFlowERC721', // _name
+        1 * 10 ** 18,   // _initialSupply
+        'AnyFlowERC20', // _name
         'AF',           // _symbol
     ] as const;
 
-    const { deploymentTransaction } = await hre.viem.sendDeploymentTransaction("AnyFlowERC721", args);
+    const contract = await hre.viem.deployContract("AnyFlowERC20", args);
 
-    console.log(`Deployment transaction for AnyFlowERC721 broadcasted... tx: ${deploymentTransaction.hash}`);
-
-    // With AnyFlow you do not need to wait for the transaction to be mined
+    console.log(`Contract AnyFlowERC20 deployed to: ${contract.address}`);
 }
 
 main()
+    .then(() => process.exit(0));
