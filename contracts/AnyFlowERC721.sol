7de18e4f1561c73eed14c015fdecaa0f1ab11336
LucasR29 <lrigon2904@gmail.com>
2025-01-13 12:22:33 -0300

    add owner

--- contracts/AnyFlowERC721.sol
+++ contracts/AnyFlowERC721.sol
@@ -1,62 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AnyFlowERC721 is ERC721 {
-    address private _owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    modifier onlyOwner() {
-        require(msg.sender == _owner, "Caller is not the owner");
-        _;
-    }
-
+contract AnyFlowERC20 is ERC20, Ownable {
     constructor(
         address _initialOwner,
+        uint256 _initialSupply,
         string memory _name,
         string memory _symbol
-    ) ERC721(_name, _symbol) {
-        _owner = _initialOwner;
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view returns (address) {
-        return _owner;
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0), "New owner is the zero address");
-        emit OwnershipTransferred(_owner, newOwner);
-        _owner = newOwner;
-    }
-
-    /**
-     * @dev Base URI for computing {tokenURI}. If set, the resulting URI for each
-     * token will be the concatenation of the `baseURI` and the `tokenId`.
-     */
-    function _baseURI() internal view virtual override returns (string memory) {
-        return "";
-    }
-
-    /**
-     * @dev Mints `tokenId` and transfers it to `to`.
-     *
-     * Requirements:
-     *
-     * - `tokenId` must not exist.
-     * - `to` cannot be the zero address.
-     *
-     * Emits a {Transfer} event.
-     */
-    function mint(address to, uint256 tokenId) public onlyOwner {
-        _safeMint(to, tokenId);
+    ) ERC20(_name, _symbol) Ownable(msg.sender) {
+        _mint(_initialOwner, _initialSupply);
     }
 }
