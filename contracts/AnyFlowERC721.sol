7a47a776d253e3ee9914cb708db772d6296d2e55
LucasR29 <lrigon2904@gmail.com>
2025-01-13 11:55:57 -0300

    add owner

--- contracts/AnyFlowERC721.sol
+++ contracts/AnyFlowERC721.sol
@@ -2,22 +2,47 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AnyFlowERC721 is ERC721, Ownable {
+contract AnyFlowERC721 is ERC721 {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Caller is not the owner");
+        _;
+    }
+
     constructor(
-        address _initialOwner,
         string memory _name,
         string memory _symbol
-    ) ERC721(_name, _symbol) Ownable(_initialOwner) {
-        // Add additional logic here
+    ) ERC721(_name, _symbol) {
+        _owner = msg.sender;
+        emit OwnershipTransferred(address(0), msg.sender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "New owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
     }
 
     /**
      * @dev Base URI for computing {tokenURI}. If set, the resulting URI for each
      * token will be the concatenation of the `baseURI` and the `tokenId`.
      */
-    function _baseURI() view virtual returns (string memory) {
+    function _baseURI() internal view virtual override returns (string memory) {
         return "";
     }
 
@@ -31,9 +56,7 @@ contract AnyFlowERC721 is ERC721, Ownable {
      *
      * Emits a {Transfer} event.
      */
-    function mint(address to, uint256 tokenId) onlyOwner {
+    function mint(address to, uint256 tokenId) public onlyOwner {
         _safeMint(to, tokenId);
     }
-
-    // Add additional logic here
 }
